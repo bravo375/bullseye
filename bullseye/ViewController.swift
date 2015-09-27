1f9207c7c25f7ffbc3145528f57f919ac698fcdf
Todd Crone <todd.crone@perceptivesoftware.com>
2015-09-27 11:49:29 -0400

    Initial commit with basic layout of app

---
+++ bullseye/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  bullseye
+//
+//  Created by Todd Crone on 9/27/15.
+//  Copyright © 2015 Bravo375. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello", message: "Sup", preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler:nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+
+}
+
